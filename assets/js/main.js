456eda9ca8a8fcefbe7128c654570dee219a64ce
fabio98p <parisifabio98@gmail.com>
2021-04-11 23:16:44 +0200

    dischi, reso gli album in ordine per data

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,14 +10,60 @@ document.addEventListener('DOMContentLoaded', function () {
 				arrayApi: []
 			},
 			methods: {
-				emailList: function () {
-					axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
-						this.arrayApi = response.data.response
-						console.log(this.arrayApi)
-					});
+				//rubatissimo dal mio progetto lista cognomi
+				ordinamentoArray: function (array, realArray) {
+					//variabili che servono all'interno del for per il corretto funzionamento
+					var x = 0
+					var temp = ""
+
+					//for che inizia i ciclo per quanto l'arrey è lungo
+					for (let i = 0; i < array.length; i++) {
+						//while che controlla se il cognome precedente è "maggiore" di quello attuale, nel caso la condizione sia vera entra nel while
+						while (array[i - x] < array[i - x - 1]) {
+							//uso la variabile temporanea(temp) per conservare il dato che poi andro a sostituire nel arrey
+							temp = array[i - x - 1]
+							//sostituzione effettiva dei dati
+							array[i - x - 1] = array[i - x]
+							array[i - x] = temp
+							//aggiornamento della x per fare in modo che non solo venga sostituito il cognome con quello precedente ma anche che questa operazione si ripeta finche il cognome non arriva nella posizione ideale
+
+							//stessa cosa ma per l'array interessato
+							temp = realArray[i - x - 1]
+							realArray[i - x - 1] = realArray[i - x]
+							realArray[i - x] = temp
+
+							x++
+						}
+						//reset della variabile x in modo che possa essere riutilizzata nel while
+						x = 0
+					}
+					realArray.reverse()
+					return realArray
+				}
+			},
+			computed: {
+				arrayOrderedForData: function () {
+					const array = []
+					
+					var newArrayApi = this.arrayApi
+					if (this.arrayApi.length != 0) {
+						for (let i = 0; i < this.arrayApi.length; i++) {
+							array.push(this.arrayApi[i].year);
+						}
+						newArrayApi = this.ordinamentoArray(array, newArrayApi)
+					}
+					console.log(newArrayApi);
+					return newArrayApi
 				}
 			},
 
+			created: function () {
+				axios.get('https://flynn.boolean.careers/exercises/api/array/music').then((response) => {
+					this.arrayApi = response.data.response
+					console.log(this.arrayApi)
+				});
+
+			}
 		}
 	);
 });
